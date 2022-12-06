8e3de70e83e614f532a145bb4e22931f30076477
Eduardo-Vitesse <edu.dev.py@gmail.com>
2022-12-06 08:30:14 -0300

    Tarefa finalizada

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "44999552332", modelo: "Modelo 01", imei: "11111", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Uber");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Nokia(numero: "44999447117", modelo: "Modelo 02", imei: "22222", memoria: 120);
+iphone.Ligar();
+iphone.InstalarAplicativo("Spotify");
